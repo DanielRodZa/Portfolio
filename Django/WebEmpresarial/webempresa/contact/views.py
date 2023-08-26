c0dd333dea538c6553ed6299786fed5fbaa7165c
Daniel Rodriguez <94081059+DanielRodZa@users.noreply.github.com>
2023-08-26 17:24:18 -0600

    Agregados proyectos de Django: WebPersonal y WebEmpresarial

---
+++ Django/WebEmpresarial/webempresa/contact/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .forms import ContactForm
+
+# Create your views here.
+
+def contact(request):
+    contact_form = ContactForm()
+
+    if request.method == 'POST':
+        contact_form = ContactForm(data=request.POST)
+
+        if contact_form.is_valid():
+            name = request.POST.get('name','')
+            email = request.POST.get('email','')
+            content = request.POST.get('content','')
+
+            # redireccion
+
+            return redirect(reverse('contact')+'?ok')
+
+
+    return render(request, 'contact/contact.html',{'form':contact_form})
---
+++ Django/WebEmpresarial/webempresa/core/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+from django.utils.timezone import now
+from django.contrib.auth.models import User
+
+# Create your models here.

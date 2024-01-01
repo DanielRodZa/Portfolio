d9eec3f707d368bbab8b60dd50272a6e0e229f1e
DanielRodZa <mts.zavaleta@gmail.com>
2023-12-31 23:39:39 -0600

    Refactorizacion del proyecto Flatmates Bill

--- OOP_Python/Flatmates_bill/PDFGenerator.py
+++ OOP_Python/Flatmates_bill/PDFGenerator.py
@@ -1,4 +1,5 @@
 import webbrowser
+import os
 
 from fpdf import FPDF
 
@@ -9,6 +10,7 @@ class PDFReport:
     """
     def __init__(self, filename):
         self.filename = filename
+        self.border = 0
 
     def generate(self, flatmate1, flatmate2, bill):
         flatmate1_pay = f"$ {flatmate1.pays(bill=bill, flatmate2=flatmate2):.2f}"
@@ -17,22 +19,24 @@ class PDFReport:
         pdf = FPDF(orientation='P', unit='pt', format='A4')
         pdf.add_page()
 
-        pdf.image("house.png", w=30, h=30)
+        pdf.image("files/house.png", w=30, h=30)
 
         pdf.set_font(family='Times', size=24, style='B')
-        pdf.cell(w=0, h=80, txt='Flatmates Bill', border=1, align='C', ln=1)
+        pdf.cell(w=0, h=80, txt='Flatmates Bill', border=self.border, align='C', ln=1)
 
 
         pdf.set_font(family='Times', size=14, style='B')
-        pdf.cell(w=100, h=40, txt="Period: ", border=1)
-        pdf.cell(w=200, h=40, txt=bill.period, border=1, ln=1)
+        pdf.cell(w=100, h=40, txt="Period: ", border=self.border)
+        pdf.cell(w=200, h=40, txt=bill.period, border=self.border, ln=1)
 
-        pdf.cell(w=100, h=40, txt=flatmate1.name, border=1)
-        pdf.cell(w=200, h=40, txt=flatmate1_pay, border=1, ln=1)
+        pdf.cell(w=100, h=40, txt=flatmate1.name, border=self.border)
+        pdf.cell(w=200, h=40, txt=flatmate1_pay, border=self.border, ln=1)
 
-        pdf.cell(w=100, h=40, txt=flatmate2.name, border=1)
-        pdf.cell(w=200, h=40, txt=flatmate2_pay, border=1, ln=1)
+        pdf.cell(w=100, h=40, txt=flatmate2.name, border=self.border)
+        pdf.cell(w=200, h=40, txt=flatmate2_pay, border=self.border, ln=1)
 
-        pdf.output(self.filename)
+        pdf.output(f"files/{self.filename}")
+
+        os.chdir("files")
 
         webbrowser.open(self.filename)

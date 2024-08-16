c87500ef501b345308e7e1ba3b1218be40854541
Daniel Rodriguez <mts.zavaleta@gmail.com>
2024-08-16 00:29:07 -0600

    requirements.txt update

--- hacking_python/seccion1/1_1_hacking_buscadores/ninjaDorks.py
+++ hacking_python/seccion1/1_1_hacking_buscadores/ninjaDorks.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import argparse
 
 from googleSearch import GoogleSearch
 from duckduckgoSearch import DuckSearch
@@ -28,4 +29,23 @@ def main_duck():
     print(results)
 
 if __name__ == '__main__':
+    # Configuración de los argumentos del programa
+    parser = argparse.ArgumentParser(description='Esta herramienta permite hacer '
+                                                 'queries en buscadores de forma '
+                                                 'automática')
+    parser.add_argument('-q','--query',
+                        help='Especifica el dork que desea buscar\nEjemplo: '
+                             '-q \'filetype:sql "MySQL dump" (pass|password|psswd|pwd)\'',)
+    parser.add_argument('-c','--configure', action='store_true',
+                        help='Inicia el proceso de configuracion del archivo .env\nUtiliza '
+                             'esta opción sin otros argumentos para configurar las claves')
+    parser.add_argument('--start-page', type=int, default=1,
+                        help='Define la página de inicio del buscador')
+    parser.add_argument("--pages", type=int, default=1,
+                        help='Número de páginas de resultados de búsqueda')
+    parser.add_argument("--lang", type=str, default='lang_es',
+                        help='Código de idioma para los resultados.\nDefault: "lang_es"')
+    parser.add_argument('-g', '--google', action='store_true',)
+    parser.add_argument('-d', '--duck', action='store_true',)
+    args = parser.parse_args()
     main_google()

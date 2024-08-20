1d6c7fae230225e01395503b8092126fdf85f9a1
Daniel Rodriguez <mts.zavaleta@gmail.com>
2024-08-20 16:46:59 -0600

    gen_dork separada de la función main_google

--- hacking_python/seccion1/1_1_hacking_buscadores/ninjaDorks.py
+++ hacking_python/seccion1/1_1_hacking_buscadores/ninjaDorks.py
@@ -43,8 +43,30 @@ def tratamiento_resultados(resultados, fichero_json=None, fichero_html=None):
     if fichero_html is not None:
         result_processor.exportar_html(fichero_html)
 
+def generate_dork(gen_dork):
+    # Preguntar si el usuario quiere usar un modelo local u OpenAI
+    respuesta = ""
+    while respuesta.lower() not in ("y", "yes", "n", "no"):
+        respuesta = input("Quieres utilizar GPT-4 de OpenAi: (yes/no)?: ")
 
-def main_google(query, start_page, pages, lang, output_json, output_html, download, gen_dork):
+    if respuesta.lower() in ("y", "yes"):
+        # Comprobamos si esta definida la API KEY de Opena AI en el fichero .env
+        if not "OPENAI_API_KEY" in os.environ:
+            openai_config()
+            load_dotenv()
+        openai_generator = OpenAIGenerator()
+        ia_agent = IAAgent(openai_generator)
+    else:
+        print("Utilizando gpt4all y ejecutando la generación en local.\n"
+              "Esta operación puede tomar vario minutos...")
+        gpt4all_generator = GPT4AllGenerator()
+        ia_agent = IAAgent(gpt4all_generator)
+
+    respuesta = ia_agent.generate_gdork(description=gen_dork)
+    print(f"\nResultado:\n{respuesta}")
+    sys.exit(0)
+
+def main_google(query, start_page, pages, lang, output_json, output_html, download):
     # Comprobar si existe el fichero .env
     env_exists = os.path.exists('.env')
     if not env_exists:
@@ -57,29 +79,6 @@ def main_google(query, start_page, pages, lang, output_json, output_html, downlo
     # Leemos la clave API y el ID del buscador
     API_KEY_GOOGLE = os.getenv("API_KEY_GOOGLE")
     SEARCH_ENGINE_ID = os.getenv("SEARCH_ENGINE_ID")
-
-    if gen_dork:
-        # Preguntar si el usuario quiere usar un modelo local u OpenAI
-        respuesta = ""
-        while respuesta.lower() not in ("y", "yes", "n", "no"):
-            respuesta = input("Quieres utilizar GPT-4 de OpenAi: (yes/no)?: ")
-
-        if respuesta.lower() in ("y", "yes"):
-            # Comprobamos si esta definida la API KEY de Opena AI en el fichero .env
-            if not "OPENAI_API_KEY" in os.environ:
-                openai_config()
-                load_dotenv()
-            openai_generator = OpenAIGenerator()
-            ia_agent = IAAgent(openai_generator)
-        else:
-            print("Utilizando gpt4all y ejecutando la generación en local.\n"
-                  "Esta operación puede tomar vario minutos...")
-            gpt4all_generator = GPT4AllGenerator()
-            ia_agent = IAAgent(gpt4all_generator)
-
-        respuesta = ia_agent.generate_gdork(description=gen_dork)
-        print(f"\nResultado:\n{respuesta}")
-        sys.exit(0)
 
     if not query:
         print("Por favor introduzca una consulta con el comando -q."
@@ -121,10 +120,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Esta herramienta permite hacer '
                                                  'queries en buscadores de forma '
                                                  'automática')
-    parser.add_argument('-q','--query',
+    parser.add_argument('-q', '--query',
                         help='Especifica el dork que desea buscar\nEjemplo: '
-                             '-q \'filetype:sql "MySQL dump" (pass|password|psswd|pwd)\'',)
-    parser.add_argument('-c','--configure', action='store_true',
+                             '-q \'filetype:sql "MySQL dump" (pass|password|psswd|pwd)\'', )
+    parser.add_argument('-c', '--configure', action='store_true',
                         help='Inicia el proceso de configuración del archivo .env\nUtiliza '
                              'esta opción sin otros argumentos para configurar las claves')
     parser.add_argument('--start_page', type=int, default=1,
@@ -133,8 +132,8 @@ if __name__ == '__main__':
                         help='Número de páginas de resultados de búsqueda')
     parser.add_argument("--lang", type=str, default='lang_es',
                         help='Código de idioma para los resultados.\nDefault: "lang_es"')
-    parser.add_argument('-g', '--google', action='store_true',)
-    parser.add_argument('-d', '--duck', action='store_true',)
+    parser.add_argument('-g', '--google', action='store_true', )
+    parser.add_argument('-d', '--duck', action='store_true', )
     parser.add_argument('--json', type=str,
                         help='Exporta los resultado en formato json')
     parser.add_argument('--html', type=str,
@@ -143,7 +142,7 @@ if __name__ == '__main__':
                         help="Especifica las extensiones de los archivos que quieres descargar separadas por coma."
                              "Ej: --dowload 'pdf,doc,sql'"
                              "Default: 'None'")
-    parser.add_argument("-gd","--generate_dork", type=str,
+    parser.add_argument("-gd", "--generate_dork", type=str,
                         help="Genera un dork a partir de una descripción por el usuario.\n"
                              "Ej: --generate-dork 'Listado de usuarios y passwords en ficheros de texto'")
     args = parser.parse_args()
@@ -151,7 +150,10 @@ if __name__ == '__main__':
         env_config()
         print("Archivo .env configurado satisfactoriamente")
         sys.exit(1)
-
+    if args.gen_dork:
+        generate_dork(
+            gen_dork=args.generate_dork,
+        )
     if args.google:
         main_google(
             query=args.query,
@@ -160,8 +162,7 @@ if __name__ == '__main__':
             lang=args.lang,
             output_json=args.json,
             output_html=args.html,
-            download=args.download,
-            gen_dork=args.generate_dork
+            download=args.download
         )
     if args.duck:
         main_duck(
